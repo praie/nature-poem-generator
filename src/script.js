e93078285160389f330bc39b378ef8b3e0e243f5
praie <85028563+praie@users.noreply.github.com>
2024-06-18 22:26:29 +0300

    Added Api intergration and fixed some errors

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Earth our mother nature",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
-    cusor: "",
+    cursor: "",
   });
 }
 
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-input");
+  let apiKey = "4t0029f88f229dc5e5o10e3ba24fdd6a";
+
+  let context =
+    "You are a poem enthusiast and love to write short poems.Your task is to generate a 4 line basic poem in HTML and separate each line with a <br>.Sign the poem with 'SheCdes AI'inside a <strong> element at the end and not at the beginning.Do not include a title to the poem.Make sure to follow instructions";
+  let prompt = `User instructions: Generate a Nature poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
+}
 let poemGeneratorFormElement = document.querySelector("#poem-generator-form");
 poemGeneratorFormElement.addEventListener("submit", generatePoem);
